51ee55452a6145e815851471735a0351add555b1
Tom Lohan <tlohan89@gmail.com>
2019-06-04 12:49:37 +0100

    added google analytics tag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,27 +6,26 @@ import * as authActions from './states/auth.actions';
 import { Store } from '@ngrx/store';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.sass']
 })
 export class AppComponent implements OnInit {
-  title = 'crossword-app';
-  _isLoggedIn = true;
+    title = 'crossword-app';
+    _isLoggedIn = true;
 
-  get isLoggedIn() {
-    return this._authService.authenticated;
-  }
+    get isLoggedIn() {
+        return this._authService.authenticated;
+    }
 
-  get profile() {
-    return this._authService.userProfile;
-  }
+    get profile() {
+        return this._authService.userProfile;
+    }
 
-  constructor(private _authService: Auth2Service, private _router: Router, private store: Store<fromRoot.State>) {
-  }
+    constructor(private _authService: Auth2Service, private _router: Router, private store: Store<fromRoot.State>) { }
 
-  ngOnInit() {
-    this.store.dispatch(new authActions.CheckLogin());
-  }
+    ngOnInit() {
+        this.store.dispatch(new authActions.CheckLogin());
+    }
 
 }
